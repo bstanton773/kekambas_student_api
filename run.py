619f0d26bf823e88039ee0a122ba9c5b8c83f398
Brian <bstanton773@gmail.com>
2021-10-25 17:51:16 -0500

    Update again with index

--- run.py
+++ run.py
@@ -1,12 +1,11 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_cors import CORS
+from flask_cors import cross_origin
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-CORS(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
@@ -26,8 +25,12 @@ class Kekambas(db.Model):
             'last_name': self.last_name
         }
 
+@app.route('/')
+def index():
+    return 'Hello World'
 
 @app.route('/kekambas')
+@cross_origin()
 def kekambas():
     students = [k.to_dict() for k in Kekambas.query.all()]
     return jsonify(students)
